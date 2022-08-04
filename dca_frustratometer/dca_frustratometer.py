43619365bda8d2be0a55cc642abea2d18630c148
Carlos Bueno <cabb99@gmail.com>
2022-08-04 15:01:53 -0500

    Adds native energy test and decoy energy calculation

--- dca_frustratometer/dca_frustratometer.py
+++ dca_frustratometer/dca_frustratometer.py
@@ -9,9 +9,10 @@ import numpy as np
 import itertools
 import scipy.io
 
+
 def get_protein_sequence_from_pdb(pdb: str,
                                   chain: str
-                                  ) -> str :
+                                  ) -> str:
     """
     Get a protein sequence from a PDB file
     :param pdb: PDB file location
@@ -20,7 +21,7 @@ def get_protein_sequence_from_pdb(pdb: str,
     """
 
     parser = PDBParser()
-    structure=parser.get_structure('name',pdb)
+    structure = parser.get_structure('name', pdb)
     protein_residues = {'ALA', 'ARG', 'ASN', 'ASP', 'CYS',
                         'GLN', 'GLU', 'GLY', 'HIS', 'ILE',
                         'LEU', 'LYS', 'MET', 'PHE', 'PRO',
@@ -29,15 +30,17 @@ def get_protein_sequence_from_pdb(pdb: str,
                    'GLN': 'Q', 'GLU': 'E', 'GLY': 'G', 'HIS': 'H', 'ILE': 'I',
                    'LEU': 'L', 'LYS': 'K', 'MET': 'M', 'PHE': 'F', 'PRO': 'P',
                    'SER': 'S', 'THR': 'T', 'TRP': 'W', 'TYR': 'Y', 'VAL': 'V'}
-    residues=[residue for residue in structure.get_residues() if (residue.has_id('CA') and  residue.get_parent().get_id()==str(chain) and residue.resname not in [' CA','PBC'])]
-    sequence=''.join([Letter_code[r.resname] for r in residues])
+    residues = [residue for residue in structure.get_residues() if (
+                residue.has_id('CA') and residue.get_parent().get_id() == str(chain) and residue.resname not in [' CA',
+                                                                                                                 'PBC'])]
+    sequence = ''.join([Letter_code[r.resname] for r in residues])
     return sequence
 
 
 def get_distance_matrix_from_pdb(pdb: str,
                                  chain: str,
                                  method: str = 'minimum'
-                                 ) -> np.array :
+                                 ) -> np.array:
     """
     Get a residue distance matrix from a pdb protein
     :param pdb: PDB file location
@@ -59,7 +62,8 @@ def get_distance_matrix_from_pdb(pdb: str,
     elif method == 'minimum':
         selection = structure.select('protein', chain=chain)
         distance_matrix = sdist.squareform(sdist.pdist(selection.getCoords()))
-        distance_matrix = pd.DataFrame(data=distance_matrix, columns=range(len(distance_matrix)), index=range(len(distance_matrix)), dtype=float)
+        distance_matrix = pd.DataFrame(data=distance_matrix, columns=range(len(distance_matrix)),
+                                       index=range(len(distance_matrix)), dtype=float)
         residues = pd.Series(selection.getResindices()).unique()
         D = np.zeros((len(residues), len(residues))) + 1000
         for ij, ab in zip(itertools.combinations(range(len(residues)), 2), itertools.combinations(residues, 2)):
@@ -70,15 +74,16 @@ def get_distance_matrix_from_pdb(pdb: str,
             D[j, i] = d
         return D
 
+
 def load_potts_model(potts_model_file):
     return scipy.io.loadmat(potts_model_file)
+
 
 def compute_native_energy(seq: str,
                           potts_model_file: str,
                           distance_matrix: np.array,
                           distance_cutoff: typing.Union[float, None] = None,
-                          sequence_distance_cutoff: typing.Union[int, None] = None):
-
+                          sequence_distance_cutoff: typing.Union[int, None] = None) -> float:
     AA = '-ACDEFGHIKLMNPQRSTVWY'
 
     potts_model = load_potts_model(potts_model_file)
@@ -89,18 +94,63 @@ def compute_native_energy(seq: str,
     h = -potts_model['h'][range(seq_len), seq_index]
     j = -potts_model['J'][range(seq_len), :, seq_index, :][:, range(seq_len), seq_index]
 
-    mask=np.ones([seq_len,seq_len])
+    mask = np.ones([seq_len, seq_len])
     if sequence_distance_cutoff is not None:
         sequence_distance = sdist.squareform(sdist.pdist(np.arange(seq_len)[:, np.newaxis]))
         mask *= sequence_distance > sequence_distance_cutoff
     if distance_cutoff is not None:
-
-        mask *= distance_matrix > distance_cutoff
-    j_prime = j * (sequence_distance > sequence_distance_cutoff) * (distance_matrix <= distance_cutoff)
+        mask *= distance_matrix <= distance_cutoff
+    j_prime = j * mask
     energy = h.sum() + j_prime.sum() / 2
     return energy
 
-def compute_frustration(with_attribution=True):
+
+def compute_singleresidue_decoy_energy(seq: str,
+                         potts_model_file: str,
+                         distance_matrix: np.array,
+                         distance_cutoff: typing.Union[float, None] = None,
+                         sequence_distance_cutoff: typing.Union[int, None] = None) -> np.array:
+    AA = '-ACDEFGHIKLMNPQRSTVWY'
+
+    potts_model = load_potts_model(potts_model_file)
+
+    seq_index = np.array([AA.find(aa) for aa in seq])
+    seq_len = len(seq_index)
+
+    # Create decoys
+    decoys = np.repeat(np.repeat(np.array(seq_index)[np.newaxis, np.newaxis, :], 21, 1), seq_len, 0)
+    for i in range(21):
+        decoys[range(seq_len), i, range(seq_len)] = i
+    # Position of mutation, Mutation, position in sequence
+    mut_pos, mut_aa, seq_pos = np.meshgrid(range(seq_len), range(21), range(seq_len), indexing='ij')
+
+    # Compute energy
+    h_decoy = -potts_model['h'][seq_pos, decoys]
+    j_decoy = -potts_model['J'][seq_pos, :, decoys, :][mut_pos, mut_aa, :, seq_pos, decoys]
+    mask = np.ones([seq_len, seq_len])
+    if sequence_distance_cutoff is not None:
+        sequence_distance = sdist.squareform(sdist.pdist(np.arange(seq_len)[:, np.newaxis]))
+        mask *= sequence_distance > sequence_distance_cutoff
+    if distance_cutoff is not None:
+        mask *= distance_matrix <= distance_cutoff
+    j_decoy_prime = j_decoy * mask
+    decoy_energy = h_decoy.sum(axis=-1) + j_decoy_prime.sum(axis=-1).sum(axis=-1) / 2
+    return decoy_energy
+
+
+def compute_aa_freq(seq_index):
+    return np.array([(seq_index == i).sum() for i in range(21)])
+
+def compute_singleresidue_frustration(decoy_energy, native_energy, aa_freq=None):
+    if aa_freq is None:
+        aa_freq = np.ones(21)
+    mean_energy = (aa_freq * decoy_energy).sum(axis=1) / aa_freq.sum()
+    std_energy = np.sqrt(((aa_freq * (decoy_energy - mean_energy[:, np.newaxis]) ** 2) / aa_freq.sum()).sum(axis=1))
+    frustration = (native_energy - mean_energy) / std_energy
+    return frustration
+
+
+def compute_mutational_frustration():
     """
     Placeholder function to show example docstring (NumPy format).
 
@@ -122,14 +172,14 @@ def compute_frustration(with_attribution=True):
         quote += "\n\t- Adapted from Henry David Thoreau"
     return quote
 
-#Function if script invoked on its own
+# Function if script invoked on its own
 def main():
     seq = get_protein_sequence_from_pdb('examples/data/1l63.pdb', 'A')
     distance_matrix = get_distance_matrix_from_pdb('examples/data/1l63.pdb', 'A')
-    e = compute_native_energy(seq, 'examples/data/PottsModel1l63A.mat', distance_matrix, distance_cutoff=4, sequence_distance_cutoff=0)
+    e = compute_native_energy(seq, 'examples/data/PottsModel1l63A.mat', distance_matrix,
+                              distance_cutoff=4, sequence_distance_cutoff=0)
     print(e)
 
 
 if __name__ == "__main__":
     main()
-
--- tests/test_dca_frustratometer.py
+++ tests/test_dca_frustratometer.py
@@ -4,12 +4,19 @@ Unit and regression test for the dca_frustratometer package.
 
 # Import package, test suite, and other packages as needed
 import sys
-
 import pytest
-
 import dca_frustratometer
+import numpy as np
 
 
 def test_dca_frustratometer_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "dca_frustratometer" in sys.modules
+
+def test_compute_native_energy():
+    seq = dca_frustratometer.get_protein_sequence_from_pdb('examples/data/1l63.pdb', 'A')
+    distance_matrix = dca_frustratometer.get_distance_matrix_from_pdb('examples/data/1l63.pdb', 'A')
+    e = dca_frustratometer.compute_native_energy(seq, 'examples/data/PottsModel1l63A.mat', distance_matrix,
+                                                 distance_cutoff=4, sequence_distance_cutoff=0)
+    assert np.round(e,4) == -92.7688
+
