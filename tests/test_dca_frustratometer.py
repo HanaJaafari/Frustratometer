f7be7a5f59c755a00cd246ea73e5245053287bff
Carlos Bueno <cabb99@gmail.com>
2022-08-04 15:14:14 -0500

    Adds requirements

--- tests/test_dca_frustratometer.py
+++ tests/test_dca_frustratometer.py
@@ -18,5 +18,5 @@ def test_compute_native_energy():
     distance_matrix = dca_frustratometer.get_distance_matrix_from_pdb('examples/data/1l63.pdb', 'A')
     e = dca_frustratometer.compute_native_energy(seq, 'examples/data/PottsModel1l63A.mat', distance_matrix,
                                                  distance_cutoff=4, sequence_distance_cutoff=0)
-    assert np.round(e,4) == -92.7688
+    assert np.round(e, 4) == -92.7688
 
