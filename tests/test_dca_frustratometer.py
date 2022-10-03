566d6e404a6398f19d6e576cf7ebb3652b70bd02
Carlos Bueno <cabb99@gmail.com>
2022-10-03 15:01:29 -0500

    Reformats file

--- tests/test_dca_frustratometer.py
+++ tests/test_dca_frustratometer.py
@@ -10,17 +10,17 @@ import numpy as np
 import logging
 
 
-
-
 def test_create_pfam_database():
     alignments_path = dca_frustratometer.create_pfam_database(url='https://ftp.ebi.ac.uk/pub/databases/Pfam'
                                                                   '/current_release/Pfam-A.dead.gz',
                                                               name='test')
-    assert (alignments_path/'Unknown.sto').exists() is False
+    assert (alignments_path / 'Unknown.sto').exists() is False
+
 
 def test_dca_frustratometer_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "dca_frustratometer" in sys.modules
+
 
 def test_functional_compute_native_energy():
     seq = dca_frustratometer.get_protein_sequence_from_pdb('examples/data/1l63.pdb', 'A')
@@ -30,20 +30,25 @@ def test_functional_compute_native_energy():
     e = dca_frustratometer.compute_native_energy(seq, potts_model, mask)
     assert np.round(e, 4) == -92.7688
 
+
 def test_OOP_compute_native_energy():
     pdb_file = 'examples/data/1l63.pdb'
     chain = 'A'
     potts_model_file = 'examples/data/PottsModel1l63A.mat'
-    model = dca_frustratometer.PottsModel.from_potts_model_file(potts_model_file, pdb_file, chain,  distance_cutoff=4, sequence_cutoff=0)
+    model = dca_frustratometer.PottsModel.from_potts_model_file(potts_model_file, pdb_file, chain, distance_cutoff=4,
+                                                                sequence_cutoff=0)
     e = model.native_energy()
     assert np.round(e, 4) == -92.7688
+
 
 def test_scores():
     pdb_file = 'examples/data/1l63.pdb'
     chain = 'A'
     potts_model_file = 'examples/data/PottsModel1l63A.mat'
-    model = dca_frustratometer.PottsModel.from_potts_model_file(potts_model_file, pdb_file, chain, distance_cutoff=4, sequence_cutoff=0)
+    model = dca_frustratometer.PottsModel.from_potts_model_file(potts_model_file, pdb_file, chain, distance_cutoff=4,
+                                                                sequence_cutoff=0)
     assert np.round(model.scores()[30, 40], 5) == -0.03384
+
 
 def test_compute_singleresidue_decoy_energy():
     aa_x = 5
@@ -62,6 +67,7 @@ def test_compute_singleresidue_decoy_energy():
     decoy_energy = dca_frustratometer.compute_decoy_energy(seq, potts_model, mask, 'singleresidue')
     assert (decoy_energy[pos_x, aa_x] - test_energy) ** 2 < 1E-16
 
+
 def test_compute_mutational_decoy_energy():
     aa_x = 5
     pos_x = 30
@@ -77,7 +83,7 @@ def test_compute_mutational_decoy_energy():
     seq = [aa for aa in seq]
     seq[pos_x] = AA[aa_x]
     seq[pos_y] = AA[aa_y]
-    seq=''.join(seq)
-    test_energy= dca_frustratometer.compute_native_energy(seq, potts_model, mask)
-    decoy_energy = dca_frustratometer.compute_decoy_energy(seq, potts_model, mask,'mutational')
+    seq = ''.join(seq)
+    test_energy = dca_frustratometer.compute_native_energy(seq, potts_model, mask)
+    decoy_energy = dca_frustratometer.compute_decoy_energy(seq, potts_model, mask, 'mutational')
     assert (decoy_energy[pos_x, pos_y, aa_x, aa_y] - test_energy) ** 2 < 1E-16
