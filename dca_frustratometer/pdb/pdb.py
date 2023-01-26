b5e1401a12ea28999c7424294af6c426f5a75469
HanaJaafari <hkj1@rice.edu>
2023-01-26 13:16:32 -0600

    Merge with main branch

--- dca_frustratometer/pdb/pdb.py
+++ dca_frustratometer/pdb/pdb.py
@@ -65,28 +65,44 @@ def get_sequence(pdb_file: str,
 
 def get_distance_matrix(pdb_file: str, 
                         chain: str, 
-                        method: str = 'minimum'
+                        method: str = 'CB'
                         ) -> np.array:
     """
-    Get a residue distance matrix from a pdb protein
-    :param pdb_file: PDB file location
-    :param chain: chain name of PDB file to get sequence
-    :param method: method to calculate the distance between residue [minimum, CA, CB]
-    :return: distance matrix
+    Calculate the distance matrix of the specified atoms in a PDB file.
+    
+    Parameters:
+        pdb_file (str): The path to the PDB file.
+        chain (str): The chainID or chainIDs (space separated) of the protein.
+        method (str): The method to use for calculating the distance matrix. 
+                      Defaults to 'CB', which uses the CB atom for all residues except GLY, which uses the CA atom. 
+                      Other options are 'CA' for using only the CA atom, 
+                      and 'minimum' for using the minimum distance between all atoms in each residue.
+    
+    Returns:
+        np.array: The distance matrix of the selected atoms.
+    
+    Raises:
+        IndexError: If the selection of atoms is empty.
     """
-    '''Returns the distance matrix of the aminoacids on a sequence. The distance used is
-    the minimum distance between two residues (for example the distance of the atoms on a H-bond)'''
+    
     structure = prody.parsePDB(pdb_file)
+    chain_selection = '' if chain is None else f' and chain {chain}'
     if method == 'CA':
-        selection = structure.select('protein and name CA', chain=chain)
+        selection = structure.select('protein and name CA' + chain_selection)
+        if len(selection) == 0:
+            raise IndexError('Empty selection')
         distance_matrix = sdist.squareform(sdist.pdist(selection.getCoords()))
         return distance_matrix
     elif method == 'CB':
-        selection = structure.select('protein and (name CB) or (resname GLY and name CA)', chain=chain)
+        selection = structure.select('(protein and (name CB) or (resname GLY and name CA))' + chain_selection)
+        if len(selection) == 0:
+            raise IndexError('Empty selection')
         distance_matrix = sdist.squareform(sdist.pdist(selection.getCoords()))
         return distance_matrix
     elif method == 'minimum':
-        selection = structure.select('protein', chain=chain)
+        selection = structure.select('protein' + chain_selection)
+        if len(selection) == 0:
+            raise IndexError('Empty selection')
         distance_matrix = sdist.squareform(sdist.pdist(selection.getCoords()))
         resids = selection.getResindices()
         residues = pd.Series(resids).unique()
