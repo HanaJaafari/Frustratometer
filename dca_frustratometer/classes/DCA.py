a22a654a7f1339c3b9e70b0f6ef35950ae34b612
HanaJaafari <hkj1@rice.edu>
2023-01-27 14:45:03 -0600

    Inherit Frustratometer Class

--- dca_frustratometer/classes/DCA.py
+++ dca_frustratometer/classes/DCA.py
@@ -15,6 +15,7 @@ from .. import align
 from .. import frustration
 from .. import pfam
 
+from .Frustratometer import Frustratometer
 __all__=['PottsModel']
 ##################
 # PFAM functions #
@@ -22,7 +23,10 @@ __all__=['PottsModel']
 
 
 # Class wrapper
-class PottsModel:
+class PottsModel(Frustratometer):
+
+    def __init__(self, pdb_file:str,chain:str=None,sequence:str= None,sequence_cutoff:typing.Union[float, None] = None,distance_cutoff:typing.Union[float, None] = None):
+        super().__init__(pdb_file, chain,sequence,sequence_cutoff,distance_cutoff)
 
     @classmethod
     def from_potts_model_file(cls,
@@ -50,10 +54,6 @@ class PottsModel:
         self.aa_freq = frustration.compute_aa_freq(self.sequence)
         self.contact_freq = frustration.compute_contact_freq(self.sequence)
         self.mask = frustration.compute_mask(self.distance_matrix, self.distance_cutoff, self.sequence_cutoff)
-
-        # Initialize slow properties
-        self._native_energy = None
-        self._decoy_fluctuation = {}
         return self
 
     @classmethod
@@ -81,10 +81,6 @@ class PottsModel:
         self.aa_freq = frustration.compute_aa_freq(self.sequence)
         self.contact_freq = frustration.compute_contact_freq(self.sequence)
         self.mask = frustration.compute_mask(self.distance_matrix, self.distance_cutoff, self.sequence_cutoff)
-
-        # Initialize slow properties
-        self._native_energy = None
-        self._decoy_fluctuation = {}
         return self
 
     @classmethod
@@ -132,10 +128,6 @@ class PottsModel:
         self._filtered_alignment_file=filter.convert_and_filter_alignment(self.alignment_file,self.download_all_alignment_files,self.alignment_files_directory)
         self.aa_freq = frustration.compute_aa_freq(self.sequence)
         self.contact_freq = frustration.compute_contact_freq(self.sequence)
-
-        # Initialize slow properties
-        self._native_energy = None
-        self._decoy_fluctuation = {}
         return self
 
     @classmethod
@@ -180,10 +172,6 @@ class PottsModel:
         self._filtered_alignment_file=filter.convert_and_filter_alignment(self.alignment_file,self.download_all_alignment_files,self.alignment_files_directory)
         self.aa_freq = frustration.compute_aa_freq(self.sequence)
         self.contact_freq = frustration.compute_contact_freq(self.sequence)
-
-        # Initialize slow properties
-        self._native_energy = None
-        self._decoy_fluctuation = {}
         return self
 
 
@@ -203,40 +191,6 @@ class PottsModel:
 
         # compute DCA scores summarized by Frobenius norm and average product corrected
         potts_model = plmdca_inst.get_potts_model()
-
-    @property
-    def sequence(self):
-        return self._sequence
-    
-    # Set a new sequence in case someone needs to calculate the energy of a diferent sequence with the same structure
-    @sequence.setter
-    def sequence(self, value):
-        assert len(value) == len(self._sequence)
-        self._sequence = value
-
-    @property
-    def pdb_file(self):
-        return str(self._pdb_file)
-
-    # @pdb_file.setter
-    # def pdb_file(self, value):
-    #     self._pdb_file = Path(value)
-
-    @property
-    def pdb_name(self, value):
-        """
-        Returns PDBid from pdb name
-        """
-        assert self._pdb_file.exists()
-        return self._pdb_file.stem
-
-    @property
-    def chain(self):
-        return self._chain
-
-    # @chain.setter
-    # def chain(self, value):
-    #     self._chain = value
 
     @property
     def pfamID(self, value):
@@ -284,179 +238,3 @@ class PottsModel:
     # @alignment_output_file.setter
     # def alignment_output_file(self, value):
     #     self._alignment_output_file = value
-
-    @property
-    def sequence_cutoff(self):
-        return self._sequence_cutoff
-
-    @sequence_cutoff.setter
-    def sequence_cutoff(self, value):
-        self.mask = frustration.compute_mask(self.distance_matrix, self.distance_cutoff, self.sequence_cutoff)
-        self._sequence_cutoff = value
-        self._native_energy = None
-        self._decoy_fluctuation = {}
-
-    @property
-    def distance_cutoff(self):
-        return self._distance_cutoff
-
-    @distance_cutoff.setter
-    def distance_cutoff(self, value):
-        self.mask = frustration.compute_mask(self.distance_matrix, self.distance_cutoff, self.sequence_cutoff)
-        self._distance_cutoff = value
-        self._native_energy = None
-        self._decoy_fluctuation = {}
-
-    @property
-    def distance_matrix_method(self):
-        return self._distance_matrix_method
-
-    @distance_matrix_method.setter
-    def distance_matrix_method(self, value):
-        self.distance_matrix = pdb.get_distance_matrix(self._pdb_file, self._chain, value)
-        self.mask = frustration.compute_mask(self.distance_matrix, self.distance_cutoff, self.sequence_cutoff)
-        self._distance_matrix_method = value
-        self._native_energy = None
-        self._decoy_fluctuation = {}
-
-    @property
-    def potts_model_file(self):
-        return self._potts_model_file
-
-    @potts_model_file.setter
-    def potts_model_file(self, value):
-        if value == None:
-            print("Generating PDB alignment using Jackhmmer")
-            align.create_alignment_jackhmmer(self.sequence, self.pdb_name,
-                                       output_file="dcaf_{}_alignment.sto".format(self.pdb_name))
-            filter.convert_and_filter_alignment(self.pdb_name)
-            dca.matlab.compute_plm(self.pdb_name)
-            raise ValueError("Need to generate potts model")
-        else:
-            self.potts_model = dca.matlab.load_potts_model(value)
-            self._potts_model_file = value
-            self._native_energy = None
-            self._decoy_fluctuation = {}
-
-    @property
-    def potts_model(self):
-        return self._potts_model
-
-    @potts_model.setter
-    def potts_model(self, value):
-        self._potts_model = value
-        self._potts_model_file = None
-        self._native_energy = None
-        self._decoy_fluctuation = {}
-
-    def native_energy(self, sequence=None):
-        if sequence is None:
-            if self._native_energy:
-                return self._native_energy
-            else:
-                return frustration.compute_native_energy(self.sequence, self.potts_model, self.mask)
-        else:
-            return frustration.compute_native_energy(sequence, self.potts_model, self.mask)
-
-    def sequences_energies(self, sequences, split_couplings_and_fields=False):
-        return frustration.compute_sequences_energy(sequences, self.potts_model, self.mask, split_couplings_and_fields)
-
-    def fields_energy(self, sequence=None):
-        if sequence is None:
-            sequence=self._sequence
-        return frustration.compute_fields_energy(sequence, self.potts_model, self.mask)
-
-    def couplings_energy(self, sequence=None):
-        if sequence is None:
-            sequence=self._sequence
-        return frustration.compute_couplings_energy(sequence, self.potts_model, self.mask)
-        
-
-
-    def decoy_fluctuation(self, kind='singleresidue'):
-        if kind in self._decoy_fluctuation:
-            return self._decoy_fluctuation[kind]
-        if kind == 'singleresidue':
-            fluctuation = frustration.compute_singleresidue_decoy_energy_fluctuation(self.sequence, self.potts_model, self.mask)
-        elif kind == 'mutational':
-            fluctuation = frustration.compute_mutational_decoy_energy_fluctuation(self.sequence, self.potts_model, self.mask)
-        elif kind == 'configurational':
-            fluctuation = frustration.compute_configurational_decoy_energy_fluctuation(self.sequence, self.potts_model, self.mask)
-        elif kind == 'contact':
-            fluctuation = frustration.compute_contact_decoy_energy_fluctuation(self.sequence, self.potts_model, self.mask)
-
-        else:
-            raise Exception("Wrong kind of decoy generation selected")
-        self._decoy_fluctuation[kind] = fluctuation
-        return self._decoy_fluctuation[kind]
-
-    def decoy_energy(self, kind='singleresidue'):
-        return self.native_energy() + self.decoy_fluctuation(kind)
-
-    def scores(self):
-        return frustration.compute_scores(self.potts_model)
-
-    def frustration(self, kind='singleresidue', aa_freq=None, correction=0):
-        decoy_fluctuation = self.decoy_fluctuation(kind)
-        if kind == 'singleresidue':
-            if aa_freq is not None:
-                aa_freq = self.aa_freq
-            return frustration.compute_single_frustration(decoy_fluctuation, aa_freq, correction)
-        elif kind in ['mutational', 'configurational', 'contact']:
-            if aa_freq is not None:
-                aa_freq = self.contact_freq
-            return frustration.compute_pair_frustration(decoy_fluctuation, aa_freq, correction)
-
-    def plot_decoy_energy(self, kind='singleresidue'):
-        native_energy = self.native_energy()
-        decoy_energy = self.decoy_energy(kind)
-        if kind == 'singleresidue':
-            frustration.plot_singleresidue_decoy_energy(decoy_energy, native_energy)
-
-    def roc(self):
-        return frustration.compute_roc(self.scores(), self.distance_matrix, self.distance_cutoff)
-
-    def plot_roc(self):
-        frustration.plot_roc(self.roc())
-
-    def auc(self):
-        """Computes area under the curve of the receiver-operating characteristic.
-           Function intended"""
-        return frustration.compute_auc(self.roc())
-
-    def vmd(self, single='singleresidue', pair='mutational', aa_freq=None, correction=0, max_connections=100):
-        tcl_script = frustration.write_tcl_script(self.pdb_file, self.chain,
-                                      self.frustration(single, aa_freq=aa_freq, correction=correction),
-                                      self.frustration(pair, aa_freq=aa_freq, correction=correction),
-                                      max_connections=max_connections)
-        frustration.call_vmd(self.pdb_file, tcl_script)
-
-    def view_frustration(self, single='singleresidue', pair='mutational', aa_freq=None, correction=0, max_connections=100):
-        import numpy as np
-        import py3Dmol
-        pdb_filename = self.pdb_file
-        pair_frustration=self.frustration(pair)*np.triu(self.mask)
-        residues=np.arange(len(self.sequence))
-        r1, r2 = np.meshgrid(residues, residues, indexing='ij')
-        sel_frustration = np.array([r1.ravel(), r2.ravel(), pair_frustration.ravel()]).T
-        minimally_frustrated = sel_frustration[sel_frustration[:, -1] < -0.78]
-        frustrated = sel_frustration[sel_frustration[:, -1] > 1]
-        
-        view = py3Dmol.view(js='https://3dmol.org/build/3Dmol.js')
-        view.addModel(open(pdb_filename,'r').read(),'pdb')
-
-        view.setBackgroundColor('white')
-        view.setStyle({'cartoon':{'color':'white'}})
-        
-        for i,j,f in frustrated:
-            view.addLine({'start':{'chain':'A','resi':[str(i+1)]},'end':{'chain':'A','resi':[str(j+1)]},
-                        'color':'red', 'dashed':False,'linewidth':3})
-        
-        for i,j,f in minimally_frustrated:
-            view.addLine({'start':{'chain':'A','resi':[str(i+1)]},'end':{'chain':'A','resi':[str(j+1)]},
-                        'color':'green', 'dashed':False,'linewidth':3})
-
-        view.zoomTo(viewer=(0,0))
-
-        return view
-
