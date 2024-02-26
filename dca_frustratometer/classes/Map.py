f74eb007f7eb5f236106d21eda165ec43e1cba52
Carlos Bueno <cabb99@gmail.com>
2024-02-26 13:10:53 -0600

    Corrects substitution_matrix for alignment

--- dca_frustratometer/classes/Map.py
+++ dca_frustratometer/classes/Map.py
@@ -15,7 +15,6 @@ class Map():
         prev_i, prev_j = path[0]
         start=0
         for curr_i, curr_j in path[1:]:
-            print(curr_i,curr_j,start,total_size)
             ixs[0,start:start+curr_i-prev_i]=np.arange(prev_i,curr_i)
             ixs[1,start:start+curr_j-prev_j]=np.arange(prev_j,curr_j)
             start=start+max(curr_i-prev_i,curr_j-prev_j)
@@ -23,12 +22,12 @@ class Map():
         return cls(ixs)
 
     @classmethod
-    def from_sequences(cls, sequence_a, sequence_b, match_score=2, mismatch_score=-1, 
+    def from_sequences(cls, sequence_a, sequence_b, substitution_matrix='BLOSUM62', match_score=2, mismatch_score=-1, 
                        open_gap_score=-0.5, extend_gap_score=-0.1, target_end_gap_score=-0.01, query_end_gap_score=-0.01):
         
         aligner=Align.PairwiseAligner()
         aligner.match_score = match_score
-        aligner.substitution_matrix = Align.substitution_matrices.load("BLOSUM62")
+        aligner.substitution_matrix = Align.substitution_matrices.load(substitution_matrix)
         aligner.mismatch_score = mismatch_score
         aligner.open_gap_score = open_gap_score
         aligner.extend_gap_score = extend_gap_score
@@ -49,8 +48,6 @@ class Map():
             return ''.join(seq[self.map_array[0]][self.map_array[1]>-1]) #Second sequence to first sequence
         else:
             if len(sequence)!=self.seq2_len:
-                print(sequence)
-                print(self.map_array)
                 raise IndexError(f'Sequence length ({len(sequence)}) does not match map ({self.seq2_len})')
                 
             return ''.join(seq[self.map_array[1]][self.map_array[0]>-1]) #First sequence to second sequence
