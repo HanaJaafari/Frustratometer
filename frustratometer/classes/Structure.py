9f70a93afa60ebfafc7608df2190376cf10f53bf
MengyanWang <mw88@rice.edu>
2024-04-04 15:27:27 -0500

    Changes pipe to comma on Union

--- frustratometer/classes/Structure.py
+++ frustratometer/classes/Structure.py
@@ -13,7 +13,7 @@ residue_names=[]
 class Structure:
     
     @classmethod
-    def full_pdb(cls,pdb_file: str, chain: Union[str|None]=None, aligned_sequence: str = None, filtered_aligned_sequence: str = None,
+    def full_pdb(cls,pdb_file: str, chain: Union[str,None]=None, aligned_sequence: str = None, filtered_aligned_sequence: str = None,
                 distance_matrix_method:str = 'CB', pdb_directory: str=os.getcwd(), repair_pdb:bool = False):
 
         """
