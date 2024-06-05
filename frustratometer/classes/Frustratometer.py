a8aaef2fdbcd4c9388163983a5a10d27e85898c6
Finley Clark <fc36@ws1808.ctbp.rice.edu>
2024-06-05 18:18:45 -0500

    added sequence argument back to vmd function but raise warning if sequence passed to vmd is different from self.sequence

--- frustratometer/classes/Frustratometer.py
+++ frustratometer/classes/Frustratometer.py
@@ -9,6 +9,7 @@ import scipy.spatial.distance as sdist
 import typing
 #Import other modules
 from .. import frustration
+import logging
 
 __all__=['PottsModel']
 ##################
@@ -129,10 +130,15 @@ class Frustratometer:
            Function intended"""
         return frustration.compute_auc(self.roc())
 
-    def vmd(self, single:typing.Union[str,np.array] = 'singleresidue', pair:typing.Union[str,np.array] = 'mutational',
+    def vmd(self, sequence: str = None, single:typing.Union[str,np.array] = 'singleresidue', pair:typing.Union[str,np.array] = 'mutational',
              aa_freq:np.array = None, correction:int = 0, max_connections:typing.Union[int,None] = None, movie_name=None):
         if sequence is None:
             sequence=self.sequence
+        elif sequence.strip() != self.sequence.strip(): 
+            logging.warning("The value of the self.sequence property of your Frustratometer object differs\n\
+                    from the sequence that was passed to this vmd function. Proceeding further may not\n\
+                    perform the computation that you intend to perform.")
+        
 
         tcl_script = frustration.write_tcl_script(self.pdb_file, self.chain, self.mask, self.distance_matrix, self.distance_cutoff,
                                       -self.frustration(kind=single, sequence=sequence, aa_freq=aa_freq, correction=correction),
